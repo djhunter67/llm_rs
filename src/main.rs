2de05c8759c93a2692b19feafa1077e3764c192b
Christerpher Hunter <djhunter67@gmail.com>
2024-03-31 22:03:53 -0400

    wip: gpu doesn't have enough memory for 6B parameter models

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,91 @@
+use std::path::PathBuf;
+
 use rust_bert::{
-    gpt_neo::{
-        GptNeoConfigResources, GptNeoMergesResources, GptNeoModelResources, GptNeoVocabResources,
-    },
+    gpt_j::{GptJConfigResources, GptJMergesResources, GptJVocabResources},
+    // gpt_neo::{GptNeoConfigResources, GptNeoMergesResources, GptNeoVocabResources},
     pipelines::{
         common::{ModelResource, ModelType},
         text_generation::{TextGenerationConfig, TextGenerationModel},
     },
-    resources::RemoteResource,
+    resources::{LocalResource, RemoteResource},
 };
 
 fn main() {
-    let cuda = tch::Cuda::is_available();
+    // let cuda = tch::Cuda::is_available();
 
-    println!("\nCUDA?: {cuda}\n");
+    // println!("\nCUDA?: {cuda}\n");
 
-    let model_resource = Box::new(RemoteResource::from_pretrained(
-        GptNeoModelResources::GPT_NEO_2_7B,
-    ));
+    // let cuda_info = tch::Cuda::device_count();
+
+    // println!("\nNUM OF CUDA DEVICES: {cuda_info}\n");
+
+    // let more_cuda_info = tch::Device::Cuda(1);
+
+    // let more_cuda_info = tch::Device::Cuda(0);
+
+    // println!("\nMORE CUDA INFO: {more_cuda_info:?}\n");
+
+    let model_resource = Box::new(LocalResource {
+        // local_path: PathBuf::from("/home/djhunter67/.BUILDS/rust_model.ot"),
+        // local_path: PathBuf::from("/home/djhunter67/.BUILDS/rust_model_1_3B.ot"),
+        local_path: PathBuf::from("/home/djhunter67/.BUILDS/model_gptj-6B.ot"),
+    });
 
     let config_resource = Box::new(RemoteResource::from_pretrained(
-        GptNeoConfigResources::GPT_NEO_2_7B,
+        // GptNeoConfigResources::GPT_NEO_1_3B,
+        GptJConfigResources::GPT_J_6B,
     ));
 
     let vocab_resource = Box::new(RemoteResource::from_pretrained(
-        GptNeoVocabResources::GPT_NEO_2_7B,
+        // GptNeoVocabResources::GPT_NEO_1_3B,
+        GptJVocabResources::GPT_J_6B,
     ));
 
     let merges_resource = Box::new(RemoteResource::from_pretrained(
-        GptNeoMergesResources::GPT_NEO_2_7B,
+        // GptNeoMergesResources::GPT_NEO_1_3B,
+        GptJMergesResources::GPT_J_6B,
     ));
 
     let generation_config = TextGenerationConfig {
-        model_type: ModelType::GPTNeo,
-        model_resource: ModelResource::Torch(model_resource),
-        config_resource,
-        vocab_resource,
-        merges_resource: Some(merges_resource),
-        num_beams: 5,
+        // model_type: ModelType::GPTNeo,
+        model_type: ModelType::GPTJ,
+        model_resource: ModelResource::Torch(model_resource.clone()),
+        config_resource: config_resource.clone(),
+        vocab_resource: vocab_resource.clone(),
+        merges_resource: Some(merges_resource.clone()),
+        num_beams: 2,
         no_repeat_ngram_size: 2,
-        max_length: Some(300),
+        max_length: Some(500),
+        device: tch::Device::Cuda(0),
         ..Default::default()
     };
 
-    let model = TextGenerationModel::new(generation_config).unwrap();
+    let model = match TextGenerationModel::new(generation_config) {
+        Ok(model) => model,
+        Err(e) => {
+            println!("Due to: {e}\nFalling back to CPU...");
+            TextGenerationModel::new(TextGenerationConfig {
+                // model_type: ModelType::GPTNeo,
+                model_type: ModelType::GPTJ,
+                model_resource: ModelResource::Torch(model_resource),
+                config_resource,
+                vocab_resource,
+                merges_resource: Some(merges_resource),
+                num_beams: 2,
+                no_repeat_ngram_size: 2,
+                max_length: Some(500),
+                device: tch::Device::Cpu,
+                ..Default::default()
+            })
+            .unwrap()
+        }
+    };
 
     loop {
+        println!("\nEnter a prompt: \n");
         let mut line = String::new();
         std::io::stdin().read_line(&mut line).unwrap();
+        println!("\nProcessing...\n\n");
 
         let split = line.split('/').collect::<Vec<&str>>();
 
